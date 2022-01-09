2cd429be56b9d0b6abcad7e5817f3a8a327af8ce
JHenrique <jhenriquess26@gmail.com>
2022-01-09 19:12:04 -0300

    Configurações iniciais

---
+++ src/core/assert/assert.js
@@ -0,0 +1,14 @@
+function assert(value, description) {
+    console.log(description);
+
+    if(!value){
+        //throw new Error('Invalid Value');
+        console.log('Invalid Value');
+        return false;
+    }else{
+        console.log('Success!');
+        return true;
+    }
+}
+
+module.exports = assert;
