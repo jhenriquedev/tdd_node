f48eb34546b9d8703aca26e054e8dbab4ab2d4e4
JHenrique <jhenriquess26@gmail.com>
2022-01-14 11:11:39 -0300

    Testes com Jest

--- src/core/assert/assert.js
+++ src/core/assert/assert.js
@@ -1,12 +1,7 @@
-function assert(value, description) {
-    //console.log(description);
-
+function assert(value) {
     if(!value){
-        //throw new Error('Invalid Value');
-        //console.log('Invalid Value');
         return false;
     }else{
-        //console.log('Success!');
         return true;
     }
 }
